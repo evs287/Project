60c630b26e3c31613cbfc7530d95faf0e2303cd5
kathya3 <31925082+kathya3@users.noreply.github.com>
2017-10-25 18:57:41 +0100

    example website

--- jquery.js
+++ jquery.js
@@ -1,9 +1,3 @@
-/*$('h1').css('font-size', '80px')*/
-
-/*$('h1').css('font-size', '8px')*/
-
-/*$('h1').css('text-align', 'right')*/
-
-/*$('h1').fadeOut()*/
-
-/*$('h1').fadeIn()*/
+$("#heart-trigger").click(function () {
+    $("li").toggleClass("visible");
+ });
